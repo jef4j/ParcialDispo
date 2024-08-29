33492218cc67a7b0206a0f533fded6dc68475b80
Jefferson Arrieta Duran <jefardu7@gmail.com>
2024-08-29 00:24:48 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  categories: string[] = [];
+  products: any[] = [];
+  selectedCategory: string = '';
+
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit() {
+    this.loadCategories();
+    this.loadProducts();
+  }
+
+  loadCategories() {
+    this.productService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  loadProducts(category?: string) {
+    if (category) {
+      this.productService.getProductsByCategory(category).subscribe(data => {
+        this.products = data;
+      });
+    } else {
+      this.productService.getAllProducts().subscribe(data => {
+        this.products = data;
+      });
+    }
+  }
+
+  onCategoryChange(event: any) {
+    this.selectedCategory = event.target.value;
+    this.loadProducts(this.selectedCategory);
+  }
+
+  viewProductDetail(id: number) {
+    this.router.navigate(['/product', id]);
+  }
+
+  goToCart() {
+    this.router.navigate(['/cart']);
+  }
 }
